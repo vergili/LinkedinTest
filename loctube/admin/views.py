5863b39bffc51d441a25217c92364413cbff307b
vergili <vergili@cern.ch>
2014-10-08 03:26:34 -0500

    update

--- loctube/admin/views.py
+++ loctube/admin/views.py
@@ -44,41 +44,44 @@ def linkedin():
 
         error_message = ""
 
-        try:
+        
 
-            json_url_part1= "https://api.linkedin.com/v1/people-search:(people:(id,public-profile-url,educations),num-results)?company-name=" + company_name[0]
+        json_url_part1= "https://api.linkedin.com/v1/people-search:(people:(id,public-profile-url,educations),num-results)?company-name=" + company_name[0]
                         
-            access_token = "AQVN-e8YJh0EXXAb-zSl2Kvl6D7vzFoPvHdGUbnBd--1yxF8t9Jcbox8qphhHL56kfsZzDdgJey8JEPuNitDzuejSxG-CyLuzXfbmwAchJ9F6aHyZSnWSekS7oazpO0WdNUlyyVlMM3HWX1YvwsRvd7sGELG_sseMtS8eWYSiLDKO7gvQQ8"             
+        access_token = "AQUNX3c0Z6trcnVPhljVOSXuHCwURVhY6dIWs1UZ4LCOn8gs_jrvKfUPpQPUlITswN-7_H6sJq3RCbzPoi3XFWnmHI05BhHoH6EBo2qYtcnrBoV9RVmPQMWrY74tITBQk3ds-ON5uknhjueyrHvmCYBLbUD6JD0C0DJC2MWWgfwINkM58cA"             
             
-            json_url_part2 = json_url_part1+ "&count=20&format=json&oauth2_access_token="+access_token
+        json_url_part2 = json_url_part1+ "&count=20&format=json&oauth2_access_token="+access_token
             
-            linkedin_json = requests.get(json_url_part2)
-            # Convert it to a Python dictionary
-            linkedin_data = json.loads(linkedin_json.text)
+        linkedin_json = requests.get(json_url_part2)
+        # Convert it to a Python dictionary
+        linkedin_data = json.loads(linkedin_json.text)
 
-            numResults = int(linkedin_data["numResults"])
-            loopTo = (numResults / 20)
+        numResults = int(linkedin_data["numResults"])
+        loopTo = (numResults / 20)
 
-            if (numResults % 20) > 0: 
-                loopTo +=1
+        if (numResults % 20) > 0: 
+            loopTo +=1
 
             
-            for i in range(0,loopTo):
+        for i in range(0,loopTo):
             
-                data = None 
-                if i == 0: 
+            data = None 
+            if i == 0: 
                          
-                    data = linkedin_data
-                else:                   
+                data = linkedin_data
+            else:                   
                    
-                    json_url_part2 = json_url_part1 + "&start="+ str(i*20)+"&count=20&format=json&oauth2_access_token="+access_token
-                    linkedin_json = requests.get(json_url_part2)
-                    linkedin_data = json.loads(linkedin_json.text)
-                    data = linkedin_data
+                json_url_part2 = json_url_part1 + "&start="+ str(i*20)+"&count=20&format=json&oauth2_access_token="+access_token
+                linkedin_json = requests.get(json_url_part2)
+                linkedin_data = json.loads(linkedin_json.text)
+                data = linkedin_data
 
-                for item in data['people']['values']:
+            for item in data['people']['values']:
+
+                try:
 
                     if item['id'] == "private": continue
+
 
                     public_pro_url = item['publicProfileUrl']
 
@@ -88,8 +91,13 @@ def linkedin():
                     public_html =PyQuery(public_pro_url); 
 
 
-                    linkedin.name      = public_html(".given-name").text() + " " + public_html(".family-name").text()
-                    linkedin.heading   = public_html(".headline-title.title").text()
+                    linkedin.name      = public_html(".full-name").text() 
+
+                    heading  = public_html(".headline-title.title").text()
+                    if heading =="": 
+                        heading  = public_html("#headline").text()
+                    linkedin.heading   = heading
+
                     linkedin.location  = public_html(".locality").text()
                     linkedin.summary   = public_html(".description.summary").text()
           
@@ -101,9 +109,9 @@ def linkedin():
                         education_dict = {}
 
                         education_dict = { "school":public_html(selector+" .summary").text(), 
-                                           "degree": public_html(selector+" .major").text(),
-                                           "dtstart": public_html(selector+" .dtstart").text(),
-                                           "dtend":  public_html(selector+" .dtend").text() }
+                                            "degree": public_html(selector+" .major").text(),
+                                            "dtstart": public_html(selector+" .dtstart").text(),
+                                            "dtend":  public_html(selector+" .dtend").text() }
                      
                         educations_list.append(education_dict)
 
@@ -114,9 +122,9 @@ def linkedin():
                         position_dict = {}
 
                         position_dict = { "title":public_html(selector+" .title").text(), 
-                                           "company": public_html(selector+" .org").text(),
-                                           "dtstart": public_html(selector+" .dtstart").text(),
-                                           "dtend":  public_html(selector+" .dtend").text() }
+                                            "company": public_html(selector+" .org").text(),
+                                            "dtstart": public_html(selector+" .dtstart").text(),
+                                            "dtend":  public_html(selector+" .dtend").text() }
                      
                         positions_list.append(position_dict)
 
@@ -129,9 +137,9 @@ def linkedin():
                     db.session.add(linkedin)
                     db.session.commit()
 
-        except Exception as inst:
-            error_message += str(inst) + " - "
-            pass
+                except Exception as inst:
+                    error_message += str(inst) + " - "
+                    pass
 
 
         flash('people has been crawled. Exception'+error_message  , 'success')
