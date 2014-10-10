ea89f5d7f17075cd98b20833af42ded3a004e02c
vergili <vergili@cern.ch>
2014-10-10 06:48:06 -0500

    update for public crawl

--- loctube/admin/views.py
+++ loctube/admin/views.py
@@ -28,6 +28,11 @@ def index():
 
 
 
+########################################################################################
+###                                                                                  ###
+###                       Crawl via public company page                              ###
+###                                                                                  ###
+########################################################################################
 @admin.route('/linkedin', methods=['GET', 'POST'])
 @login_required
 @admin_required
@@ -40,11 +45,143 @@ def linkedin():
     
     if request.method == 'POST':
 
+        formData = form.company.data.split('\r\n')
+
+        companyName = formData[0]
+        error_message = ""
+
+        linkedin_company_url = "https://www.linkedin.com/company/"+companyName
+
+        companyHtml = PyQuery(url=linkedin_company_url);
+
+        firstLinks = companyHtml(".discovery-photo");
+
+        listOfpeople = [a.attrib['href'].split("?")[0]  for a in firstLinks];
+
+        newList =[]
+
+
+        oldListOfpeople = []
+        i = 0
+        while i < len(listOfpeople):
+
+            memberHtml = PyQuery(url=listOfpeople[i] );
+
+            memberPublicUrl = listOfpeople[i]
+
+            listOfpeople.remove(listOfpeople[i])
+
+            newLinks = memberHtml(".with-photo strong a")
+            if len(newLinks) == 0:
+                newLinks = memberHtml(".insights-browse-map h4 a")
+
+            current = memberHtml(".org.summary").text()
+            if current == "" or companyName not in current.lower():
+                current = memberHtml("#overview-summary-current li").text()
+            elif current == "" or companyName not in current.lower():
+                current = memberHtml(".summary-current li").text()
+
+        
+            past    = memberHtml(".past").text()
+            if past == "" or companyName not in past.lower():
+                past = memberHtml("#overview-summary-past").text()
+            elif past == "" or companyName not in past.lower():
+                past = memberHtml(".summary-past li").text()
+
+
+            if companyName not in current.lower() and companyName not in past.lower() :  continue
+    
+            newList = [a.attrib['href'].split("?")[0]  for a in newLinks];
+
+    
+            yeniList = [a for a in newList if a not in listOfpeople and a not in oldListOfpeople ]
+  
+            oldListOfpeople.append(memberPublicUrl)
+    
+            listOfpeople += yeniList 
+
+            linkedin = Linkedin()
+
+            public_html =PyQuery(memberPublicUrl); 
+
+            linkedin.name      = public_html(".full-name").text() 
+
+            heading  = public_html(".headline-title.title").text()
+            if heading =="": 
+                heading  = public_html("#headline").text()
+            linkedin.heading   = heading
+
+            linkedin.location  = public_html(".locality").text()
+            linkedin.summary   = public_html(".description.summary").text()
+          
+
+            educations_list = [] 
+            for k in range(0, len(public_html(".education .summary"))):
+                selector = ".education:eq(" +str(k)+ ")"
+                    
+                education_dict = {}
+
+                education_dict = { "school":public_html(selector+" .summary").text(), 
+                                    "degree": public_html(selector+" .major").text(),
+                                    "dtstart": public_html(selector+" .dtstart").text(),
+                                    "dtend":  public_html(selector+" .dtend").text() }
+                     
+                educations_list.append(education_dict)
+
+            positions_list = [] 
+            for k in range(0, len(public_html(".experience .title"))):
+                selector = ".experience:eq(" +str(k)+ ")" 
+
+                position_dict = {}
+
+                position_dict = { "title":public_html(selector+" .title").text(), 
+                                    "company": public_html(selector+" .org").text(),
+                                    "dtstart": public_html(selector+" .dtstart").text(),
+                                    "dtend":  public_html(selector+" .dtend").text() }
+                     
+                positions_list.append(position_dict)
+
+
+
+            linkedin.education = json.dumps(educations_list)
+            linkedin.positions = json.dumps(positions_list)
+
+                
+            db.session.add(linkedin)
+            db.session.commit()
+
+
+        flash('people has been crawled. Exception: '  , 'success')
+
+        return render_template( "admin/linkedin.html",form=form, active='linkedin')
+
+
+
+
+
+####################################################################################
+###                                                                              ###
+###                       Crawl via linkedin API                                 ###
+###                                                                              ###
+####################################################################################
+@admin.route('/linkedin2', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def linkedin2():
+    form = LinkedinForm()
+    error = None
+    if request.method == 'GET':
+        return render_template( "admin/linkedin.html", form=form, active='linkedin')
+
+    
+    if request.method == 'POST':
+
         company_name = form.company.data.split('\r\n')
 
         error_message = ""
 
-        
+        linke = requests.get("http://www.linkedin.com/company/1596318")
+
 
         json_url_part1= "https://api.linkedin.com/v1/people-search:(people:(id,public-profile-url,educations),num-results)?company-name=" + company_name[0]
                         
@@ -142,6 +279,9 @@ def linkedin():
                     pass
 
 
-        flash('people has been crawled. Exception'+error_message  , 'success')
+        flash('people has been crawled. Exception: '+error_message  , 'success')
 
         return render_template( "admin/linkedin.html",form=form, active='linkedin')
+
+
+
